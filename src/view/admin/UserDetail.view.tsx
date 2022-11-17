2a50ca0c6704a5ea30606a49b5f4e58c83afaba2
silviaoh <silvia.jisu.oh@gmail.com>
2022-11-17 16:16:28 +0900

    Style: 사용자 상세 정보 CSS

--- src/view/admin/UserDetail.view.tsx
+++ src/view/admin/UserDetail.view.tsx
@@ -17,28 +17,28 @@ const UserDetailView = () => {
       <AdminMainLayout>
         <MainHeader />
         <AdminContentLayout>
-          <h1>사용자 정보</h1>
-          <section>
-            <h1>이름</h1>
-            <div>{user?.name}</div>
-            <h1>성별</h1>
-            <div>{user?.gender_origin}</div>
-            <h1>생년월일</h1>
-            <div>{user?.birth_date}</div>
-            <h1>주소</h1>
-            <div>{user?.address}</div>
-            <h1>상세 주소</h1>
-            <div>{user?.detail_address}</div>
-            <h1>이메일</h1>
-            <div>{user?.email}</div>
-            <h1>핸드폰</h1>
-            <div>{user?.phone_number}</div>
-            <h1>연령</h1>
-            <div>{user?.age}</div>
-            <h1>가입일</h1>
-            <div>{user?.created_at}</div>
-            <h1>최근 로그인</h1>
-            <div>{user?.last_login}</div>
+          <h1 className="title-main">사용자 정보</h1>
+          <section className="grid grid-cols-6 grid-rows-3 border border-gray-300 text-xs">
+            <h1 className="table-cell-title_border_br">이름</h1>
+            <div className="table-cell_border_br">{user?.name}</div>
+            <h1 className="table-cell-title_border_br">성별</h1>
+            <div className="table-cell_border_br">{user?.gender_origin}</div>
+            <h1 className="table-cell-title_border_br font-table-title">생년월일</h1>
+            <div className="table-cell_border_b">{user?.birth_date}</div>
+            <h1 className="table-cell-title_border_br">주소</h1>
+            <div className="table-cell_border_br">{user?.address}</div>
+            <h1 className="table-cell-title_border_br ">상세 주소</h1>
+            <div className="table-cell_border_br">{user?.detail_address}</div>
+            <h1 className="table-cell-title_border_br">이메일</h1>
+            <div className="table-cell_border_b">{user?.email}</div>
+            <h1 className="table-cell-title_border_br">핸드폰</h1>
+            <div className="table-cell_border_br">{user?.phone_number}</div>
+            <h1 className="table-cell-title_border_br">연령</h1>
+            <div className="table-cell_border_br">{user?.age}</div>
+            <h1 className="table-cell-title_border_br">가입일</h1>
+            <div className="table-cell_border_b">{user?.created_at}</div>
+            <h1 className="table-cell-title_border_r">최근 로그인</h1>
+            <div className="table-cell_border_r">{user?.last_login}</div>
           </section>
         </AdminContentLayout>
       </AdminMainLayout>
